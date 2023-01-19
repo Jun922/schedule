b231d8e063c6bbf4c778e4cc697ddf17d1ec1c0d
Jun922 <junjunchi620@gmail.com>
2023-01-19 22:08:01 +0900

    str obj

--- models.py
+++ models.py
@@ -4,3 +4,6 @@ class Event(models.Model):
   start_date = models.DateField()
   end_date = models.DateField()
   event_name = models.CharField(max_length=200)
+
+  def __str__(self):
+    return self.event_name
