8b5035151ba4ac648844eebfea0d381dddec42f4
Jun922 <junjunchi620@gmail.com>
2023-01-24 23:28:02 +0900

    css

--- models.py
+++ models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
 class Event(models.Model):
-  start_date = models.DateField()
-  end_date = models.DateField()
-  event_name = models.CharField(max_length=200)
+  start_date = models.DateField('予定開始日')
+  end_date = models.DateField('予定終了日')
+  event_name = models.CharField('タイトル', max_length=200)
   info = models.TextField('予定内容', max_length=50)
 
   def __str__(self):
