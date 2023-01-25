8cd1ceeb3b27e711b8a26f844491c5df53b379b1
Jun922 <junjunchi620@gmail.com>
2023-01-25 15:50:08 +0900

    html form fix

--- models.py
+++ models.py
@@ -4,7 +4,7 @@ class Event(models.Model):
   start_date = models.DateField('予定開始日')
   end_date = models.DateField('予定終了日')
   event_name = models.CharField('タイトル', max_length=200)
-  info = models.TextField('予定内容', max_length=50)
+  info = models.TextField('予定内容', max_length=100)
 
   def __str__(self):
     return self.event_name
